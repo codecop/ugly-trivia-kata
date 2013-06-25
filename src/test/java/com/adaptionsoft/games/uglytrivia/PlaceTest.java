b7280c9a67a7088b0da672df8cfa1e5a0631a817
Thomas Sundberg <tsu@kth.se>
2013-06-25 18:00:02 +0200

    Placed the extracted class Place under test

---
+++ src/test/java/com/adaptionsoft/games/uglytrivia/PlaceTest.java
@@ -0,0 +1,36 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class PlaceTest {
+
+    private Place place = new Place();
+
+    @Test
+    public void shouldAdvance() {
+        place.advanceCurrentPlayerBy(3);
+
+        assertEquals(3, place.getPlace());
+    }
+
+    @Test
+    public void shouldAdvanceBelowBoundary() {
+        place.advanceCurrentPlayerBy(5);
+
+        place.advanceCurrentPlayerBy(6);
+
+        assertEquals(11, place.getPlace());
+    }
+
+    @Test
+    public void shouldAdvanceRoundBoundary() {
+        place.advanceCurrentPlayerBy(11);
+
+        place.advanceCurrentPlayerBy(3);
+
+        assertEquals(2, place.getPlace());
+    }
+
+}
