2f7f02a3d1aa227d85a0fc16d716780559efb859
Thomas Sundberg <tsu@kth.se>
2014-01-10 16:42:32 +0100

    Wrong answer should send player to penalty box

--- src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -2,11 +2,32 @@ package com.adaptionsoft.games.uglytrivia;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 public class GameTest {
     @Test
-    public void bringMeUnderTest() {
-        fail("Please, bring me under test.");
+    public void wrongAnswerShouldSendPlayerToPenaltyBox() {
+        Players players = new Players();
+        players.add("Lonely Tom");
+        Player player = players.getCurrentPlayer();
+
+        Game game = new Game(players, null);
+        game.wrongAnswer();
+
+        assertTrue("Player should be in penalty box", player.isInPenaltyBox());
     }
+
+           /*
+        a coin should not be added
+
+        turn is over
+
+        test output
+
+         */
+
+
+
+
+
 }
