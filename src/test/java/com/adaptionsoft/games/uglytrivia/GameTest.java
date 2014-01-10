af2d04fd8f7d9bee7240dc0a1a1817ff66bf81c0
Thomas Sundberg <tsu@kth.se>
2014-01-10 17:06:30 +0100

    Wrong answer should not get a coin

--- src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -3,22 +3,39 @@ package com.adaptionsoft.games.uglytrivia;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 public class GameTest {
     @Test
     public void wrongAnswerShouldSendPlayerToPenaltyBox() {
-        Players players = new Players();
-        players.add("Lonely Tom");
-        Player player = players.getCurrentPlayer();
+        Player lonelyTom = new Player("Lonely Tom");
+        Game game = createGameWithSinglePlayer(lonelyTom);
 
-        Game game = new Game(players, null);
         game.wrongAnswer();
 
-        assertTrue("Player should be in penalty box", player.isInPenaltyBox());
+        assertTrue("Player should be in penalty box", lonelyTom.isInPenaltyBox());
     }
 
+    @Test
+    public void wrongAnswerShouldNotGetACoin(){
+        Player player = mock(Player.class);
+        Game game = createGameWithSinglePlayer(player);
+
+        game.wrongAnswer();
+
+        verify(player, never()).answeredCorrect();
+    }
+
+    private Game createGameWithSinglePlayer(Player player) {
+        Players players = new Players();
+        players.add(player);
+        return new Game(players, null);
+    }
+
+
            /*
-        a coin should not be added
 
         turn is over
 
