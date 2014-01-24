ba4654cf8250921bc2f9c5a1d01dd443a8d2958d
Thomas Sundberg <tsu@kth.se>
2014-01-24 19:31:16 +0100

    Test that player is can leave penalty box when lucky, but ignored because of defect

--- src/test/java/com/adaptionsoft/games/uglytrivia/GameCorrectAnswerTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/GameCorrectAnswerTest.java
@@ -1,8 +1,11 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.hamcrest.core.Is;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class GameCorrectAnswerTest {
@@ -32,4 +35,19 @@ public class GameCorrectAnswerTest {
 
         verify(players).changeCurrentPlayer();
     }
+
+    @Test
+    @Ignore("known defect in code")
+    public void correctAnswerShouldGetPlayerOutOfPenaltyBoxIfLucky() {
+        Player playerInPenaltyBox = mock(Player.class);
+        when(playerInPenaltyBox.isInPenaltyBox()).thenReturn(true);
+        when(playerInPenaltyBox.isGettingOutOfPenaltyBox()).thenReturn(true);
+        Game game = createGameWithSinglePlayer(playerInPenaltyBox);
+
+        game.correctAnswer();
+
+        // verify(playerInPenaltyBox).exitPenaltyBox();
+    }
+
+    // right answer with enough previous couns should end game
 }
