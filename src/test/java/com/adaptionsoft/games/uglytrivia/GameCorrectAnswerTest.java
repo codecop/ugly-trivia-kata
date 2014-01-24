e0ff9ec98c4174b08623c5589cb9e5c407819cfb
Thomas Sundberg <tsu@kth.se>
2014-01-24 18:46:23 +0100

    Test that correct answer gets a coin

--- src/test/java/com/adaptionsoft/games/uglytrivia/GameCorrectAnswerTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/GameCorrectAnswerTest.java
@@ -1,13 +1,25 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 public class GameCorrectAnswerTest {
 
     @Test
-    public void placeMeUnderTest(){
-        fail("Place Game.correctAnswer under test");
+    public void correctAnswerShouldGetACoin() {
+        Player player = mock(Player.class);
+        Game game = createGameWithSinglePlayer(player);
+
+        game.correctAnswer();
+
+        verify(player, times(1)).answeredCorrect();
+    }
+
+    private Game createGameWithSinglePlayer(Player player) {
+        Players players = new Players();
+        players.add(player);
+        return new Game(players, null);
     }
 }
