0eb67511dee23273a3c67b88b2a2a833f25fa0a4
Thomas Sundberg <tsu@kth.se>
2014-03-06 18:29:24 +0100

    Test that player is asked a question and that question is of proper category

--- src/test/java/com/adaptionsoft/games/uglytrivia/GamePlayTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/GamePlayTest.java
@@ -2,8 +2,15 @@ package com.adaptionsoft.games.uglytrivia;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GamePlayTest {
 
@@ -18,6 +25,42 @@ public class GamePlayTest {
         game.playCurrentPlayer(eyesOfDice);
 
         verify(players).advanceBy(eyesOfDice);
+    }
+
+    @Test
+    public void playerShouldBeAskedQuestion() {
+        new SystemOutCapture() {
+            {
+
+                Players players = mock(Players.class);
+                Questions questions = mock(Questions.class);
+                when(questions.nextFor(any(Category.class))).thenReturn("Pop Question 1");
+                Game game = GameBuilder.createGameWith(players, questions);
+
+                int anyEye = 0;
+                game.playCurrentPlayer(anyEye);
+
+                assertThat(capturedSystemOutLines(), hasItems("Pop Question 1" ));
+
+            }
+
+            private List<String> capturedSystemOutLines() {
+                return Arrays.asList(capturedSystemOut().split(cr()));
+            }
+        }.resetSystemOut();
+    }
+
+    @Test
+    public void playerShouldBeAskedQuestionBelongingToCategoryOfPlace() {
+        Players players = mock(Players.class);
+        when(players.currentCategory()).thenReturn(Category.POP);
+        Questions questions = mock(Questions.class);
+        Game game = GameBuilder.createGameWith(players, questions);
+
+        int anyEye = 7;
+        game.playCurrentPlayer(anyEye);
+
+        verify(questions).nextFor(Category.POP);
     }
 
 }
