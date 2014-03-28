ea93da8035b0bb53e6d93279d5642195b9b4a130
Peter Kofler <peter.kofler@code-cop.org>
2014-03-28 17:18:27 +0100

    use proper Assert, do not use old 3.x Assert class

--- src/test/java/com/adaptionsoft/games/uglytrivia/PlaceTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/PlaceTest.java
@@ -2,7 +2,7 @@ package com.adaptionsoft.games.uglytrivia;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class PlaceTest {
 
